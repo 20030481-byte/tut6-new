404247ac7b67a77e9ce28b4dc0a25f866fea1b99
20030481-byte <20030481@students.koi.edu.au>
2026-04-22 00:24:12 +1000

    Initial commit

---
+++ tut6/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tut6.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Welcome(string name, int numtimes)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numtimes;
+            return View();
+        }
+    }
+}
---
+++ tut6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tut6.MvcApplication" Language="C#" %>
